453604ed611b9a316a759a8ccbefeb50eb10eb1f
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-03-12 07:01:02 +0600

    implement video loading functionality and enhance video card display

--- js/main.js
+++ js/main.js
@@ -4,18 +4,13 @@ const loaCategories = () => {
         .then((data) => displayCategories(data.categories));
 };
 
-
 function loadVideos() {
     // https://openapi.programming-hero.com/api/phero-tube/videos
 
     fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
         .then((response) => response.json())
         .then((data) => displayVideos(data.videos));
-
-
 }
-
-
 
 function displayCategories(data) {
     const categoryContainar = document.getElementById("category-containar");
@@ -23,17 +18,12 @@ function displayCategories(data) {
         const categoryDiv = document.createElement("div");
         categoryDiv.innerHTML = `
             <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
-        `
+        `;
         categoryContainar.appendChild(categoryDiv);
     }
 }
 
-
-
-
-
-
-// const video = {
+// {
 //     "category_id": "1001",
 //     "video_id": "aaaa",
 //     "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
@@ -52,29 +42,44 @@ function displayCategories(data) {
 //     "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
 // }
 
-
 const displayVideos = (videos) => {
     const videoContainar = document.getElementById("video-containar");
     videos.forEach((video) => {
-
         // console.log(video);
 
         const videoCard = document.createElement("div");
         videoCard.innerHTML = `
-            <div class="card">
-                <img class="" src="${video.thumbnail}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${video.title}</h5>
-                    <p class="card-text">${video.description}</p>   
+            
+                <div class="card bg-base-100">
+                    <figure class="relative">
+                        <img class="h-[250px] w-full object-cover" src="${video.thumbnail}" alt="" />
+                        <span class="absolute bottom-2 right-2 bg-black text-white rounded py-1 px-2 text-sm">3hrs 56
+                            min ago</span>
+                    </figure>
+                    <div class="flex gap-3 px-0 py-5">
+                        <div>
+                            <div class="avatar">
+                                <div class="ring-primary ring-offset-base-100 w-10 rounded-full ring ring-offset-2">
+                                    <img
+                                        src="${video.authors[0].profile_picture}" />
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <h2 class="text-base font-bold">${video.title}</h2>
+                            <p class="text-gray-400 text-sm flex gap-1 items-center">${video.authors[0].profile_name}
+                                <img class="h-5 w-5" src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" alt="">
+                            </p>
+                            <p class="text-gray-400 text-sm">${video.others.views} Views</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
+
         `;
         videoContainar.appendChild(videoCard);
-
-
     });
-}
+};
 
 // Function call
 loaCategories();
-loadVideos();
+
